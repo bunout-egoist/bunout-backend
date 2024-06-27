c96f16ef1a233c6cc04c723f4dbefc624f6f777a
Hagoeun <sera1193@naver.com>
2024-06-27 16:22:45 +0900

    first commit

---
+++ src/main/java/dough/DoughApplication.java
@@ -0,0 +1,13 @@
+package dough;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoughApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoughApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dough/DoughApplicationTests.java
@@ -0,0 +1,13 @@
+package dough;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoughApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
