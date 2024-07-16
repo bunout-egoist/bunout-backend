20ab4fb9f2f342bc5a5058357346472b0d6143d2
Hagoeun <sera1193@naver.com>
2024-07-16 14:04:19 +0900

    [Fix] Cannot construct instance 에러 수정

--- src/main/java/dough/global/exception/ExceptionResponse.java
+++ src/main/java/dough/global/exception/ExceptionResponse.java
@@ -1,12 +1,12 @@
 package dough.global.exception;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.*;
 
 @Getter
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ExceptionResponse {
 
-    private final int code;
-    private final String message;
+    private int code;
+    private String message;
 }
