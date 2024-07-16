4847114cc4af531ffd079222ddf70cc5da672d38
Hagoeun <sera1193@naver.com>
2024-07-16 13:44:33 +0900

    [Fix] GlobalExceptionHandler 클래스 확장하도록 변경
    - ResponseEntityExceptionHandler 확장하도록 변경하였음

--- src/main/java/dough/global/exception/ExceptionCode.java
+++ src/main/java/dough/global/exception/ExceptionCode.java
@@ -3,8 +3,8 @@ package dough.global.exception;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
+@RequiredArgsConstructor
 public enum ExceptionCode {
 
     INVALID_REQUEST(1000, "올바르지 않은 요청입니다."),
@@ -13,9 +13,8 @@ public enum ExceptionCode {
 
     // 2000 feedback
 
-    // 3000 quest
 
-    SERVER_ERROR(9999,"서버에서 에러가 발생하였습니다.");
+    INTERNAL_SEVER_ERROR(9999,"서버에서 에러가 발생하였습니다.");
     
     private final int code;
     private final String message;
--- src/main/java/dough/global/exception/GlobalExceptionHandler.java
+++ src/main/java/dough/global/exception/GlobalExceptionHandler.java
@@ -1,18 +1,29 @@
 package dough.global.exception;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Objects;
 
+import static dough.global.exception.ExceptionCode.INTERNAL_SEVER_ERROR;
 import static dough.global.exception.ExceptionCode.INVALID_REQUEST;
 
 @RestControllerAdvice
-public class GlobalExceptionHandler {
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            final MethodArgumentNotValidException e,
+            final HttpHeaders headers,
+            final HttpStatusCode status,
+            final WebRequest request
+    ) {
         final String errMessage = Objects.requireNonNull(e.getBindingResult().getFieldError()).getDefaultMessage();
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(INVALID_REQUEST.getCode(), errMessage));
@@ -22,5 +33,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ExceptionResponse> handleBadRequestException(final BadRequestException e) {
         return ResponseEntity.badRequest()
                 .body(new ExceptionResponse(e.getCode(), e.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(final Exception e) {
+        return ResponseEntity.internalServerError()
+                .body(new ExceptionResponse(INTERNAL_SEVER_ERROR.getCode(), INTERNAL_SEVER_ERROR.getMessage()));
     }
 }
