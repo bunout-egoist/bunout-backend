ee6bd347816f58b3f2e9771e523cc9141eb3a126
Hagoeun <sera1193@naver.com>
2024-07-18 17:16:33 +0900

    [TEST] 유저 닉네임 조회 서비스 테스트

--- src/main/java/dough/member/service/MemberService.java
+++ src/main/java/dough/member/service/MemberService.java
@@ -4,9 +4,9 @@ import dough.global.exception.BadRequestException;
 import dough.member.domain.Member;
 import dough.member.domain.repository.MemberRepository;
 import dough.member.dto.response.MemberInfoResponse;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import static dough.global.exception.ExceptionCode.NOT_FOUND_MEMBER_ID;
 
@@ -17,6 +17,7 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Transactional(readOnly = true)
     public MemberInfoResponse getMemberInfo(final Long memberId) {
         final Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new BadRequestException(NOT_FOUND_MEMBER_ID));
