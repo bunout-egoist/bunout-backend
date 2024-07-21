2161a642df936b0e98efc1f1921e3fcaa3d0e594
Hagoeun <sera1193@naver.com>
2024-07-22 00:44:20 +0900

    [TEST] 멤버 닉네임 수정 테스트 추가
    - Controller
    - Service

--- src/test/java/dough/global/member/controller/MemberControllerTest.java
+++ src/test/java/dough/global/member/controller/MemberControllerTest.java
@@ -1,12 +1,15 @@
 package dough.global.member.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dough.global.AbstractControllerTest;
 import dough.member.controller.MemberController;
+import dough.member.dto.request.MemberInfoRequest;
 import dough.member.dto.response.MemberInfoResponse;
 import dough.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -16,11 +19,11 @@ import org.springframework.test.web.servlet.ResultActions;
 import static dough.global.restdocs.RestDocsConfiguration.field;
 import static javax.management.openmbean.SimpleType.LONG;
 import static javax.management.openmbean.SimpleType.STRING;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
-import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
-import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -31,6 +34,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class MemberControllerTest extends AbstractControllerTest {
 
     // TODO 추후에 token 추가
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private MemberService memberService;
 
@@ -39,7 +46,7 @@ class MemberControllerTest extends AbstractControllerTest {
     void getMemberInfo() throws Exception {
         // given
         Long id = 1L;
-        final MemberInfoResponse memberInfoResponse = new MemberInfoResponse(id, "jjanggu");
+        final MemberInfoResponse memberInfoResponse = new MemberInfoResponse(id, "goeun");
 
         when(memberService.getMemberInfo(id))
                 .thenReturn(memberInfoResponse);
@@ -67,5 +74,49 @@ class MemberControllerTest extends AbstractControllerTest {
 
         // then
         verify(memberService).getMemberInfo(id);
+    }
+
+    @DisplayName("멤버 닉네임을 수정할 수 있다.")
+    @Test
+    void updateMemberInfo() throws Exception {
+        // given
+        Long id = 1L;
+        final MemberInfoRequest memberInfoRequest = new MemberInfoRequest("minju");
+        final MemberInfoResponse memberInfoResponse = new MemberInfoResponse(id, "minju");
+
+        when(memberService.updateMemberInfo(anyLong(), any()))
+                .thenReturn(memberInfoResponse);
+
+        final ResultActions resultActions = mockMvc.perform(put("/api/v1/members/{memberId}", id)
+                .contentType(APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(memberInfoRequest)));
+
+        // when
+        resultActions.andExpect(status().isOk())
+                .andDo(restDocs.document(
+                        pathParameters(
+                                parameterWithName("memberId")
+                                        .description("멤버 아이디")
+                        ),
+                        requestFields(
+                                fieldWithPath("nickname")
+                                        .type(STRING)
+                                        .description("멤버 닉네임")
+                                        .attributes(field("constraint", "문자열"))
+                        ),
+                        responseFields(
+                                fieldWithPath("id")
+                                        .type(LONG)
+                                        .description("멤버 아이디")
+                                        .attributes(field("constraint", "문자열")),
+                                fieldWithPath("nickname")
+                                        .type(STRING)
+                                        .description("멤버 닉네임")
+                                        .attributes(field("constraint", "문자열"))
+                        )
+                ));
+
+        // then
+        verify(memberService).updateMemberInfo(anyLong(), any());
     }
 }
--- src/test/java/dough/global/member/service/MemberServiceTest.java
+++ src/test/java/dough/global/member/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package dough.global.member.service;
 
 import dough.member.domain.Member;
 import dough.member.domain.repository.MemberRepository;
+import dough.member.dto.request.MemberInfoRequest;
 import dough.member.dto.response.MemberInfoResponse;
 import dough.member.service.MemberService;
 import org.junit.jupiter.api.DisplayName;
@@ -16,7 +17,9 @@ import java.util.Optional;
 
 import static dough.login.domain.type.SocialLoginType.KAKAO;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 @Transactional
@@ -32,7 +35,7 @@ class MemberServiceTest {
     @Test
     void getMemberInfo() {
         // given
-        final Member member = new Member(1L, "jjanggu", "0000", KAKAO, "jjanggu@mail.com", "기타", "남성", 1994, "빵");
+        final Member member = new Member(1L, "goeun", "0000", KAKAO, "goeun@mail.com", "기타", "여성", 2002, "빵");
         given(memberRepository.findById(member.getId()))
                 .willReturn(Optional.of(member));
 
@@ -41,5 +44,26 @@ class MemberServiceTest {
 
         // then
         assertThat(memberInfoResponse).usingRecursiveComparison().isEqualTo(MemberInfoResponse.from(member));
+    }
+
+    @DisplayName("멤버의 닉네임을 수정할 수 있다.")
+    @Test
+    void updateMemberInfo() {
+        // given
+        final MemberInfoRequest memberInfoRequest = new MemberInfoRequest("jjanggu");
+        final Member member = new Member(1L, "goeun", "0000", KAKAO, "goeun@mail.com", "기타", "여성", 2002, "빵");
+        member.updateMember("cheolsu");
+
+        given(memberRepository.findById(any()))
+                .willReturn(Optional.of(member));
+        given(memberRepository.save(any()))
+                .willReturn(member);
+
+        // when
+        memberService.updateMemberInfo(member.getId(), memberInfoRequest);
+
+        // then
+        verify(memberRepository).findById(any());
+        verify(memberRepository).save(any());
     }
 }
