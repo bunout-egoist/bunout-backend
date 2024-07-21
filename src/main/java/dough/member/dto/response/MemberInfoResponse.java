cf3bce9e660754ba53816289bc3e9e8d85a8bcff
Hagoeun <sera1193@naver.com>
2024-07-21 17:34:50 +0900

    [FEAT] RestDocsConfiguration에 field 추가

--- src/main/java/dough/member/dto/response/MemberInfoResponse.java
+++ src/main/java/dough/member/dto/response/MemberInfoResponse.java
@@ -7,9 +7,10 @@ import lombok.*;
 @RequiredArgsConstructor
 public class MemberInfoResponse {
 
+    private final Long id;
     private final String nickname;
 
     public static MemberInfoResponse from(final Member member) {
-        return new MemberInfoResponse(member.getNickname());
+        return new MemberInfoResponse(member.getId(), member.getNickname());
     }
 }
