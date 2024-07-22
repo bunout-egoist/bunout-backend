3ce76501fe88b06a2d7f0a229678831458b7790a
karl21-02 <manuna530@gmail.com>
2024-07-22 23:10:02 +0900

    [FEAT] findByUsername 기능 추가

--- src/main/java/dough/member/domain/repository/MemberRepository.java
+++ src/main/java/dough/member/domain/repository/MemberRepository.java
@@ -2,6 +2,9 @@ package dough.member.domain.repository;
 
 import dough.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByUsername(String username);
 }
