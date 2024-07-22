f27641bccb6294d2940ca6dd16807f162dbf0466
karl21-02 <manuna530@gmail.com>
2024-07-22 22:29:51 +0900

    [FEAT] KakaoResponse 구현 완료

--- src/main/java/dough/login/dto/response/KakaoResponse.java
+++ src/main/java/dough/login/dto/response/KakaoResponse.java
@@ -8,7 +8,7 @@ public class KakaoResponse implements OAuth2Response {
 
     public KakaoResponse(Map<String, Object> attribute) {
         // Kakao API의 응답 구조에 따라 attribute에서 필요한 정보를 추출합니다.
-        this.attribute = (Map<String, Object>) attribute.get("kakao_account");
+        this.attribute = attribute;
     }
 
     @Override
@@ -22,13 +22,19 @@ public class KakaoResponse implements OAuth2Response {
     }
     @Override
     public String getEmail() {
-
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attribute.get("kakao_account");
         return attribute.get("email").toString();
     }
 
     @Override
     public String getName() {
+        Map<String, Object> properties = (Map<String, Object>) attribute.get("properties");
+        return attribute.get("nickname").toString();
+    }
 
-        return attribute.get("name").toString();
+    @Override
+    public String getNickname() {
+        Map<String, Object> properties = (Map<String, Object>) attribute.get("properties");
+        return (String) attribute.get("nickname");
     }
 }
