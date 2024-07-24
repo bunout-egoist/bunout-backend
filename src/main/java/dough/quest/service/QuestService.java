d5f3b75d4ea692072f16b26d6c733a6356b657fe
Hagoeun <sera1193@naver.com>
2024-07-25 01:13:47 +0900

    [FEAT] 완료 퀘스트 피드백 서비스 구현

--- src/main/java/dough/quest/service/QuestService.java
+++ src/main/java/dough/quest/service/QuestService.java
@@ -1,7 +1,28 @@
 package dough.quest.service;
 
+import dough.quest.domain.repository.SelectedQuestRepository;
+import dough.quest.dto.CompletedQuestFeedbackElement;
+import dough.quest.dto.response.CompletedQuestDetailResponse;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class QuestService {
+
+    private final SelectedQuestRepository selectedQuestRepository;
+
+    public List<CompletedQuestDetailResponse> getCompletedQuestDetail(final Long memberId, final LocalDate searchDate) {
+        List<CompletedQuestFeedbackElement> completedQuestFeedbackElements = selectedQuestRepository.findCompletedQuestFeedbackByMemberIdAndSearchDate(memberId, searchDate);
+        return completedQuestFeedbackElements.stream()
+                .map(completedQuest -> CompletedQuestDetailResponse.of(
+                        completedQuest.getFeedback(),
+                        completedQuest.getQuest()
+                )).toList();
+    }
 }
