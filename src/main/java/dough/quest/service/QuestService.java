5e35dfd338d87bfed2793259a7025c5523e962cd
Hagoeun <sera1193@naver.com>
2024-07-25 16:45:45 +0900

    [FEAT] 퀘스트 업데이트 Service 구현

--- src/main/java/dough/quest/service/QuestService.java
+++ src/main/java/dough/quest/service/QuestService.java
@@ -1,12 +1,18 @@
 package dough.quest.service;
 
+import dough.global.exception.BadRequestException;
+import dough.member.domain.Member;
 import dough.quest.domain.Quest;
 import dough.quest.domain.repository.QuestRepository;
 import dough.quest.domain.type.QuestType;
 import dough.quest.dto.request.QuestRequest;
+import dough.quest.dto.request.QuestUpdateRequest;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static dough.global.exception.ExceptionCode.NOT_FOUND_MEMBER_ID;
+import static dough.global.exception.ExceptionCode.NOT_FOUND_QUEST_ID;
 
 @Service
 @RequiredArgsConstructor
@@ -25,5 +31,22 @@ public class QuestService {
         );
 
         return questRepository.save(quest).getId();
+    }
+
+    public void update(final Long questId, final QuestUpdateRequest questUpdateRequest) {
+        if (!questRepository.existsById(questId)) {
+            throw new BadRequestException(NOT_FOUND_QUEST_ID);
+        }
+
+        final QuestType questType = QuestType.getMappedQuestType(questUpdateRequest.getQuestType());
+        final Quest updateQuest = new Quest(
+                questId,
+                questUpdateRequest.getDescription(),
+                questUpdateRequest.getActivity(),
+                questType,
+                questUpdateRequest.getDifficulty()
+        );
+
+        questRepository.save(updateQuest);
     }
 }
