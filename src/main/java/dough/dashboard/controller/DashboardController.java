9709713f7e317cd0e6184526e2d49a1f2ba8df4c
Hagoeun <sera1193@naver.com>
2024-07-25 11:44:23 +0900

    [FEAT] 날짜별 달성한 퀘스트 상세 조회 컨트롤러 구현

---
+++ src/main/java/dough/dashboard/controller/DashboardController.java
@@ -0,0 +1,30 @@
+package dough.dashboard.controller;
+
+import dough.quest.dto.response.CompletedQuestDetailResponse;
+import dough.quest.service.QuestService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/dashboard")
+public class DashboardController {
+
+    public final QuestService questService;
+
+    @GetMapping("/quests/{memberId}/{searchDate}")
+    public ResponseEntity<List<CompletedQuestDetailResponse>> getCompletedQuestDetail(
+            @PathVariable("memberId") final Long memberId,
+            @PathVariable("searchDate") final LocalDate date
+    ) {
+        final List<CompletedQuestDetailResponse> detailResponse = questService.getCompletedQuestDetail(memberId, date);
+        return ResponseEntity.ok().body(detailResponse);
+    }
+}
