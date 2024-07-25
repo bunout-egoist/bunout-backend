b06802b5c14967de2ffa152559f7daaba42e676a
Hagoeun <sera1193@naver.com>
2024-07-25 12:38:05 +0900

    [FEAT] 퀘스트 저장 컨트롤러 구현

--- src/main/java/dough/quest/controller/QuestController.java
+++ src/main/java/dough/quest/controller/QuestController.java
@@ -1,11 +1,25 @@
 package dough.quest.controller;
 
+import dough.quest.dto.request.QuestRequest;
+import dough.quest.service.QuestService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/quests")
 public class QuestController {
+
+    private final QuestService questService;
+
+    @PostMapping
+    public ResponseEntity<Void> saveQuest(@RequestBody @Valid final QuestRequest questRequest) {
+        questService.save(questRequest);
+        return ResponseEntity.ok().build();
+    }
 }
