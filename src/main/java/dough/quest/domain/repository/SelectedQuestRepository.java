5fa776db3ee1bf919611fd6e20b2397115129854
Hagoeun <sera1193@naver.com>
2024-07-28 01:13:21 +0900

    [FIX] 달성한 퀘스트 조회 레포지토리 수정

--- src/main/java/dough/quest/domain/repository/SelectedQuestRepository.java
+++ src/main/java/dough/quest/domain/repository/SelectedQuestRepository.java
@@ -3,6 +3,7 @@ package dough.quest.domain.repository;
 import dough.quest.domain.SelectedQuest;
 import dough.quest.dto.CompletedQuestFeedbackElement;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,11 +12,11 @@ import java.util.List;
 
 public interface SelectedQuestRepository extends JpaRepository<SelectedQuest, Long> {
 
-    @Query(value = "SELECT DISTINCT new dough.quest.dto.CompletedQuestFeedbackElement(s.quest, s.feedback) " +
+    @Query("SELECT DISTINCT new dough.quest.dto.CompletedQuestFeedbackElement(s.quest, f) " +
             "FROM SelectedQuest s " +
-            "LEFT JOIN FETCH s.feedback " +
-            "LEFT JOIN FETCH s.quest " +
-            "WHERE s.member.id = :memberId AND s.createdAt = :date AND s.questStatus = 'COMPLETED'", nativeQuery = true)
+            "LEFT JOIN s.feedback f " +
+            "LEFT JOIN s.quest q " +
+            "WHERE s.member.id = :memberId AND FUNCTION('DATE', s.createdAt) = :date AND s.questStatus = 'COMPLETED'")
     List<CompletedQuestFeedbackElement> findCompletedQuestFeedbackByMemberIdAndDate(
             @Param("memberId") final Long memberId,
             @Param("date") final LocalDate date
