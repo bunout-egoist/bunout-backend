db691fee24d5e249b7cd8c3beb71913e483aa4de
karl21-02 <manuna530@gmail.com>
2024-07-31 13:46:51 +0900

    [FEAT] AccessTokenResponse 구현

---
+++ src/main/java/dough/login/dto/response/CreateAccessTokenResponse.java
@@ -0,0 +1,10 @@
+package dough.login.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class CreateAccessTokenResponse {
+    private String accessToken;
+}
