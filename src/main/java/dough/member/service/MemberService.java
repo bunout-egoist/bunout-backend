848a4de191b35e6053861f876ef12b01be15649a
karl21-02 <manuna530@gmail.com>
2024-07-31 13:51:14 +0900

    [FEAT] MemberService 구현

--- src/main/java/dough/member/service/MemberService.java
+++ src/main/java/dough/member/service/MemberService.java
@@ -5,9 +5,13 @@ import dough.member.domain.Member;
 import dough.member.domain.repository.MemberRepository;
 import dough.member.dto.request.MemberInfoRequest;
 import dough.member.dto.response.MemberInfoResponse;
+import dough.login.domain.type.RoleType;
+import dough.login.domain.type.SocialLoginType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 import static dough.global.exception.ExceptionCode.NOT_FOUND_MEMBER_ID;
 
@@ -33,5 +37,10 @@ public class MemberService {
         memberRepository.save(member);
 
         return MemberInfoResponse.from(member);
+    }
+
+    public Member findById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected user"));
     }
 }
