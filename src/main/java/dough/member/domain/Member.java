9edda48553a29427849e77a062cc1ab0be494727
karl21-02 <manuna530@gmail.com>
2024-07-31 14:13:02 +0900

    [FIX] 오타 수정

--- src/main/java/dough/member/domain/Member.java
+++ src/main/java/dough/member/domain/Member.java
@@ -29,7 +29,7 @@ import static lombok.AccessLevel.PROTECTED;
 @DynamicInsert
 @NoArgsConstructor(access = PROTECTED)
 @SQLDelete(sql = "UPDATE member SET status = 'DELETED' where id = ?")
-@SQLRestriction("status is 'ACTIVE'")
+@SQLRestriction("status = 'ACTIVE'")
 public class Member extends BaseEntity implements UserDetails {
 
     @Id
