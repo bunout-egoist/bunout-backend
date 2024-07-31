e04e46d0d7f03adc35c0cdee84333dd9b9a81d6c
karl21-02 <manuna530@gmail.com>
2024-07-31 13:50:56 +0900

    [FEAT] MemberRepository 구현

--- src/main/java/dough/member/domain/repository/MemberRepository.java
+++ src/main/java/dough/member/domain/repository/MemberRepository.java
@@ -1,10 +1,13 @@
 package dough.member.domain.repository;
 
+import dough.login.domain.type.SocialLoginType;
 import dough.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findBySocialLoginId(String socialLoginId);
     Optional<Member> findByEmail(String email);
-    Optional<Member> findByUsername(String username);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByNickname(String username);
 }
