76cb99504fc60cc406601915ca5c6483902fd0f7
Hagoeun <sera1193@naver.com>
2024-08-01 17:32:56 +0900

    [FEAT] 번아웃 타입 수정 컨트롤러

--- src/main/java/dough/member/controller/MemberController.java
+++ src/main/java/dough/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package dough.member.controller;
 
+import dough.member.dto.request.BurnoutTypeRequest;
 import dough.member.dto.request.MemberInfoRequest;
 import dough.member.dto.response.MemberInfoResponse;
 import dough.member.service.MemberService;
@@ -23,8 +24,17 @@ public class MemberController {
 
     @PutMapping("/{memberId}")
     public ResponseEntity<MemberInfoResponse> updateMemberInfo(@PathVariable("memberId") final Long memberId,
-                                                               @RequestBody @Valid final MemberInfoRequest memberInfoRequest) {
+                                                               @RequestBody @Valid final MemberInfoRequest memberInfoRequest
+    ) {
         final MemberInfoResponse memberInfoResponse = memberService.updateMemberInfo(memberId, memberInfoRequest);
         return ResponseEntity.ok().body(memberInfoResponse);
+    }
+
+    @PutMapping("/{memberId}")
+    public ResponseEntity<Void> changeBurnoutType(@PathVariable("memberId") final Long memberId,
+                                                  @RequestBody @Valid final BurnoutTypeRequest burnoutTypeRequest
+    ) {
+        memberService.changeBurnoutType(memberId, burnoutTypeRequest);
+        return ResponseEntity.noContent().build();
     }
 }
