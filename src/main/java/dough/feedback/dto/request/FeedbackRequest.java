83201317d1536dcee2ebb23015fdc7552a9c4a99
karl21-02 <manuna530@gmail.com>
2024-08-02 01:55:18 +0900

    [FEAT] FeedbackRequest 구현

--- src/main/java/dough/feedback/dto/request/FeedbackRequest.java
+++ src/main/java/dough/feedback/dto/request/FeedbackRequest.java
@@ -1,4 +1,23 @@
 package dough.feedback.dto.request;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FeedbackRequest {
+
+
+    private String imageUrl;
+
+    @NotBlank(message = "퀘스트 난이도를 입력해주세요.")
+    @Min(1)
+    @Max(5)
+    private Integer difficulty;
 }
