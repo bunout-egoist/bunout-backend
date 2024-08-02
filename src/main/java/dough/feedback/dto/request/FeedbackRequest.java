c064e5a9e101014b213e39d16fab8df87b385af4
karl21-02 <manuna530@gmail.com>
2024-08-02 12:06:46 +0900

    [FIX] difficulty 필드 @NotNull로 수정

--- src/main/java/dough/feedback/dto/request/FeedbackRequest.java
+++ src/main/java/dough/feedback/dto/request/FeedbackRequest.java
@@ -3,6 +3,7 @@ package dough.feedback.dto.request;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -16,8 +17,8 @@ public class FeedbackRequest {
 
     private String imageUrl;
 
-    @NotBlank(message = "퀘스트 난이도를 입력해주세요.")
     @Min(1)
     @Max(5)
+    @NotNull(message = "퀘스트 난이도를 입력해주세요.")
     private Integer difficulty;
 }
