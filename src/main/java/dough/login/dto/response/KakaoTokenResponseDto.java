c2fc60e77a00b8b5ceafaa97b9de2e282bfa966a
karl21-02 <manuna530@gmail.com>
2024-08-05 19:23:34 +0900

    [STYLE] 주석 제거

--- src/main/java/dough/login/dto/response/KakaoTokenResponseDto.java
+++ src/main/java/dough/login/dto/response/KakaoTokenResponseDto.java
@@ -6,23 +6,14 @@ import lombok.Getter;
 @Getter
 public class KakaoTokenResponseDto {
 
-    /**
-     * tokenType: bearer로 고정
-     */
     @JsonProperty("token_type")
     private String tokenType;
     @JsonProperty("access_token")
     private String accessToken;
-    /**
-     * 액세스 토큰과 ID 토큰의 만료 시간(초)
-     */
     @JsonProperty("expires_in")
     private Integer expiresIn;
     @JsonProperty("refresh_token")
     private String refreshToken;
-    /**
-     * 리프레시 토큰 만료 시간(초)
-     */
     @JsonProperty("refresh_token_expires_in")
     private Integer refreshTokenExpiresIn;
 
