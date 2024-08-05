3f7d0143b5495cb6e145fe0ec2b732d085d110d6
karl21-02 <manuna530@gmail.com>
2024-08-05 17:45:48 +0900

    [FEAT] 회원 정보 추가 입력 Controller 구현

---
+++ src/main/java/dough/login/controller/SignUpController.java
@@ -0,0 +1,33 @@
+package dough.login.controller;
+
+import dough.global.exception.ExceptionResponse;
+import dough.login.config.jwt.TokenProvider;
+import dough.login.dto.request.SignUpRequest;
+import dough.member.dto.response.MemberInfoResponse;
+import dough.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@RequiredArgsConstructor
+public class SignUpController {
+
+    private final TokenProvider tokenProvider;
+    private final MemberService memberService;
+
+    @PostMapping("/api/v1/signup/info")
+    public ResponseEntity<?> signupInfo(@RequestBody SignUpRequest signUpRequest) {
+        String accessToken = signUpRequest.getAccessToken();
+        if(!tokenProvider.validToken(accessToken)) {
+            return ResponseEntity.status(401).body(new ExceptionResponse(401, "Invalid Token"));
+        }
+
+        MemberInfoResponse memberInfoResponse = memberService.updateMemberInfo(signUpRequest);
+        return ResponseEntity.ok(memberInfoResponse);
+    }
+}
