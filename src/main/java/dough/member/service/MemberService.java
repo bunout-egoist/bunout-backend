1efc1491e1f016a13db4c3e21090494df86aa428
karl21-02 <manuna530@gmail.com>
2024-08-05 17:47:53 +0900

    [FEAT] 추가로 받은 회원 정보 업데이트
    회원 정보 추가 입력시 받은 정보들로 Member를 업데이트 해주는 기능을 구현했습니다.

--- src/main/java/dough/member/service/MemberService.java
+++ src/main/java/dough/member/service/MemberService.java
@@ -1,6 +1,9 @@
 package dough.member.service;
 
 import dough.global.exception.BadRequestException;
+import dough.global.exception.UserNotFoundException;
+import dough.login.config.jwt.TokenProvider;
+import dough.login.dto.request.SignUpRequest;
 import dough.member.domain.Member;
 import dough.member.domain.repository.MemberRepository;
 import dough.member.dto.request.BurnoutTypeRequest;
@@ -22,6 +25,7 @@ import static dough.global.exception.ExceptionCode.NOT_FOUND_MEMBER_ID;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
 
     @Transactional(readOnly = true)
     public MemberInfoResponse getMemberInfo(final Long memberId) {
@@ -58,5 +62,22 @@ public class MemberService {
                 throw new BadRequestException(ALREADY_UPDATED_BURNOUT_TYPE);
             }
         }
+    }
+
+    public MemberInfoResponse updateMemberInfo(SignUpRequest signUpRequest) {
+        String accessToken = signUpRequest.getAccessToken();
+        String socialLoginId = tokenProvider.getUserIdFromToken(accessToken);
+
+        final Member member = memberRepository.findBySocialLoginId(socialLoginId)
+                .orElseThrow(UserNotFoundException::new);
+
+        member.updateMember(
+                member.getNickname(),
+                member.getGender(),
+                member.getBirthYear(),
+                member.getOccupation()
+        );
+
+        return MemberInfoResponse.from(memberRepository.save(member));
     }
 }
