f2a41708d3f54420b028df553f09725b8d8de7ed
karl21-02 <manuna530@gmail.com>
2024-08-05 19:00:03 +0900

    [TEST] SignUpControllerTest 구현

--- src/test/java/dough/login/controller/SignUpControllerTest.java
+++ src/test/java/dough/login/controller/SignUpControllerTest.java
@@ -3,6 +3,7 @@ package dough.login.controller;
 import dough.DoughApplication;
 import dough.login.config.jwt.TokenProvider;
 import dough.login.dto.request.SignUpRequest;
+import dough.login.service.SignUpService;
 import dough.member.dto.response.MemberInfoResponse;
 import dough.member.service.MemberService;
 import org.junit.jupiter.api.BeforeEach;
@@ -34,16 +35,14 @@ public class SignUpControllerTest {
     private TokenProvider tokenProvider;
 
     @MockBean
-    private MemberService memberService;
+    private SignUpService signUpService;
 
-    private SignUpRequest validSignUpRequest;
     private MemberInfoResponse memberInfoResponse;
     private String validAccessToken;
 
     @BeforeEach
     public void setup() {
         validAccessToken = "validAccessToken";
-        validSignUpRequest = new SignUpRequest(validAccessToken, "nick", "남성", 1990, "직장인");
         memberInfoResponse = new MemberInfoResponse(1L, "nick");
     }
 
@@ -52,7 +51,7 @@ public class SignUpControllerTest {
     public void testSignupInfo_withValidToken() throws Exception {
         // Given: 유효한 토큰이 있고, 회원 가입 요청이 존재하며, 해당 요청에 대한 기대 응답이 정의됨
         Mockito.when(tokenProvider.validToken(anyString())).thenReturn(true);
-        Mockito.when(memberService.updateMemberInfo(any(SignUpRequest.class))).thenReturn(memberInfoResponse);
+        Mockito.when(signUpService.updateMemberInfo(any(SignUpRequest.class))).thenReturn(memberInfoResponse);
 
         // When: 유효한 토큰과 함께 회원 가입 정보 업데이트 요청을 보내면
         mockMvc.perform(post("/api/v1/signup/info")
