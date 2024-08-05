e36e022f76220d09a43d867b57ca7f43da381a2e
karl21-02 <manuna530@gmail.com>
2024-08-05 17:46:08 +0900

    [FEAT] 회원 정보 입력시 Request폼 구현

---
+++ src/main/java/dough/login/dto/request/SignUpRequest.java
@@ -0,0 +1,30 @@
+package dough.login.dto.request;
+
+import jakarta.validation.constraints.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SignUpRequest {
+    @NotBlank(message = "access token을 입력해주세요.")
+    private String accessToken;
+
+    @NotBlank(message = "닉네임을 입력해주세요.")
+    @Size(max = 5, message = "닉네임은 5자 이내로 입력해주세요.")
+    private String nickname;
+
+    @NotBlank(message = "성별을 입력해주세요.")
+    @Pattern(regexp = "남성|여성|기타", message = "성별은 남성, 여성, 기타 중 하나여야 합니다.")
+    private String gender;
+
+    @NotNull(message = "출생연도를 입력해주세요.")
+    private int birth_year;
+
+    @NotBlank(message = "직업을 입력해주세요.")
+    @Pattern(regexp = "학생|직장인|자영업|주부|무직|기타", message = "직업은 학생, 직장인, 자영업, 주부, 무직, 기타 중 하나여야 합니다.")
+    private String occupation;
+}
