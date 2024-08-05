27080c761ef9dd5f73b23b8a0aee4d61637980f8
karl21-02 <manuna530@gmail.com>
2024-08-05 19:24:02 +0900

    [STYLE] 밑줄 추가

--- src/main/java/dough/login/dto/response/KakaoTokenResponseDto.java
+++ src/main/java/dough/login/dto/response/KakaoTokenResponseDto.java
@@ -8,17 +8,22 @@ public class KakaoTokenResponseDto {
 
     @JsonProperty("token_type")
     private String tokenType;
+
     @JsonProperty("access_token")
     private String accessToken;
+
     @JsonProperty("expires_in")
     private Integer expiresIn;
+
     @JsonProperty("refresh_token")
     private String refreshToken;
+
     @JsonProperty("refresh_token_expires_in")
     private Integer refreshTokenExpiresIn;
 
     @JsonProperty("id_token")
     private String idToken;
+
     @JsonProperty("scope")
     private String scope;
 
