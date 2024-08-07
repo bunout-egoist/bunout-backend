43b6c98620f985efa303bb799a43b50360f9bdbf
karl21-02 <manuna530@gmail.com>
2024-08-07 19:06:35 +0900

    [FEAT] LogoutService 구현

---
+++ src/main/java/dough/logout/service/LogoutService.java
@@ -0,0 +1,49 @@
+package dough.logout.service;
+
+import dough.global.exception.BadRequestException;
+import dough.login.config.jwt.TokenProvider;
+import dough.login.domain.RefreshToken;
+import dough.login.domain.repository.RefreshTokenRepository;
+import dough.logout.dto.request.DeleteAccessTokenRequest;
+import dough.logout.dto.response.DeleteAccessTokenResponse;
+import dough.member.domain.Member;
+import dough.member.domain.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static dough.global.exception.ExceptionCode.INVALID_REQUEST;
+import static dough.global.exception.ExceptionCode.NOT_FOUND_MEMBER_ID;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutService {
+
+    private final TokenProvider tokenProvider;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public DeleteAccessTokenResponse logout(DeleteAccessTokenRequest deleteAccessTokenRequest) {
+
+        String accessToken = deleteAccessTokenRequest.getAccessToken();
+
+        if (!tokenProvider.validToken(accessToken)) {
+            throw new BadRequestException(INVALID_REQUEST);
+        }
+
+        String socialLoginId = tokenProvider.getUserIdFromToken(accessToken);
+
+        RefreshToken refreshToken = refreshTokenRepository.findBySocialLoginId(socialLoginId)
+                .orElseThrow(() -> new BadRequestException(NOT_FOUND_MEMBER_ID));
+
+        if(refreshToken != null) {
+            refreshTokenRepository.delete(refreshToken);
+        }
+
+        Member member = memberRepository.findBySocialLoginId(socialLoginId)
+                .orElseThrow(() -> new BadRequestException(NOT_FOUND_MEMBER_ID));
+
+        return DeleteAccessTokenResponse.from(member);
+    }
+}
