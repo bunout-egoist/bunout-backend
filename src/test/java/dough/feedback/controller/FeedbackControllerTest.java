630c50acf136d08280337f939be7cf91fc560096
Hagoeun <sera1193@naver.com>
2024-08-19 21:59:24 +0900

    [TEST] 피드백 테스트 수정

--- src/test/java/dough/feedback/controller/FeedbackControllerTest.java
+++ src/test/java/dough/feedback/controller/FeedbackControllerTest.java
@@ -14,13 +14,14 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.test.web.servlet.ResultActions;
 
-import static dough.feedback.fixture.FeedbackFixture.FEEDBACK1;
 import static dough.global.restdocs.RestDocsConfiguration.field;
+import static dough.member.fixture.MemberFixture.GOEUN;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
-import static org.springframework.restdocs.payload.JsonFieldType.*;
+import static org.springframework.restdocs.payload.JsonFieldType.NUMBER;
+import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
 import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
@@ -41,19 +42,20 @@ class FeedbackControllerTest extends AbstractControllerTest {
     @Test
     void createFeedback() throws Exception {
         // given
-        Long questId = FEEDBACK1.getSelectedQuest().getQuest().getId();
-        FeedbackRequest feedbackRequest = new FeedbackRequest(
-                FEEDBACK1.getImageUrl(),
-                FEEDBACK1.getDifficulty()
+        final FeedbackRequest feedbackRequest = new FeedbackRequest(
+                "png1",
+                1L,
+                5
         );
-        FeedbackResponse feedbackResponse = FeedbackResponse.from(FEEDBACK1);
+
+        final FeedbackResponse feedbackResponse = new FeedbackResponse(1, "열광형");
 
 
         when(feedbackService.createFeedback(any(), any(FeedbackRequest.class)))
                 .thenReturn(feedbackResponse);
 
         // when
-        ResultActions resultActions = mockMvc.perform(post("/api/v1/feedbacks/{questId}", questId)
+        ResultActions resultActions = mockMvc.perform(post("/api/v1/feedbacks/{memberId}", GOEUN.getId())
                 .contentType(APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(feedbackRequest)));
 
@@ -61,18 +63,32 @@ class FeedbackControllerTest extends AbstractControllerTest {
         resultActions.andExpect(status().isOk())
                 .andDo(restDocs.document(
                         pathParameters(
-                                parameterWithName("questId").description("퀘스트 아이디")
+                                parameterWithName("memberId")
+                                        .description("멤버 아이디")
                         ),
                         requestFields(
-                                fieldWithPath("imageUrl").type(STRING).description("이미지 URL").attributes(field("constraint", "문자열")),
-                                fieldWithPath("difficulty").type(NUMBER).description("난이도").attributes(field("constraint", "1-5 사이의 정수"))
+                                fieldWithPath("selectedQuestId")
+                                        .type(NUMBER)
+                                        .description("선택된 퀘스트 아이디")
+                                        .attributes(field("constraint", "양의 정수")),
+                                fieldWithPath("imageUrl")
+                                        .type(STRING)
+                                        .description("이미지 URL")
+                                        .attributes(field("constraint", "문자열")),
+                                fieldWithPath("difficulty")
+                                        .type(NUMBER)
+                                        .description("난이도")
+                                        .attributes(field("constraint", "1-5 사이의 정수"))
                         ),
                         responseFields(
-                                fieldWithPath("id").type(NUMBER).description("피드백 아이디").attributes(field("constraint", "양의 정수")),
-                                fieldWithPath("imageUrl").type(STRING).description("이미지 URL").attributes(field("constraint", "문자열")),
-                                fieldWithPath("difficulty").type(NUMBER).description("난이도").attributes(field("constraint", "1-5 사이의 정수")),
-                                fieldWithPath("memberId").type(NUMBER).description("멤버 아이디").attributes(field("constraint", "양의 정수")),
-                                fieldWithPath("selectedQuestId").type(NUMBER).description("선택된 퀘스트 아이디").attributes(field("constraint", "양의 정수"))
+                                fieldWithPath("level")
+                                        .type(NUMBER)
+                                        .description("레벨")
+                                        .attributes(field("constraint", "양의 정수")),
+                                fieldWithPath("burnoutName")
+                                        .type(STRING)
+                                        .description("번아웃 유형 이름")
+                                        .attributes(field("constraint", "문자열"))
                         )
                 ));
     }
--- src/test/java/dough/feedback/service/FeedbackServiceTest.java
+++ src/test/java/dough/feedback/service/FeedbackServiceTest.java
@@ -4,15 +4,9 @@ import dough.feedback.domain.Feedback;
 import dough.feedback.domain.repository.FeedbackRepository;
 import dough.feedback.dto.request.FeedbackRequest;
 import dough.feedback.dto.response.FeedbackResponse;
-import dough.login.domain.type.SocialLoginType;
-import dough.member.domain.Member;
+import dough.global.exception.BadRequestException;
 import dough.member.domain.repository.MemberRepository;
-import dough.quest.domain.Quest;
-import dough.quest.domain.SelectedQuest;
 import dough.quest.domain.repository.SelectedQuestRepository;
-import dough.quest.domain.type.QuestStatus;
-import dough.quest.domain.type.QuestType;
-import dough.quest.service.QuestService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -23,15 +17,17 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
-import static dough.burnout.fixture.BurnoutFixture.ENTHUSIAST;
-import static dough.keyword.fixture.KeywordFixture.INSIDE_ALONE;
-import static dough.keyword.fixture.KeywordFixture.OUTSIDE_ALONE;
-import static dough.level.fixture.LevelFixture.LEVEL1;
-import static dough.login.domain.type.RoleType.MEMBER;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static dough.feedback.fixture.FeedbackFixture.FEEDBACK1;
+import static dough.global.exception.ExceptionCode.NOT_FOUND_MEMBER_ID;
+import static dough.global.exception.ExceptionCode.NOT_FOUND_SELECTED_QUEST_ID;
+import static dough.member.fixture.MemberFixture.GOEUN;
+import static dough.quest.fixture.SelectedQuestFixture.COMPLETED_QUEST1;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.anyLong;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 @Transactional
@@ -39,9 +35,6 @@ class FeedbackServiceTest {
 
     @InjectMocks
     private FeedbackService feedbackService;
-
-    @Mock
-    private QuestService questService;
 
     @Mock
     private FeedbackRepository feedbackRepository;
@@ -54,69 +47,66 @@ class FeedbackServiceTest {
 
     @DisplayName("피드백을 성공적으로 생성할 수 있다.")
     @Test
-    void createFeedbackSuccess() {
+    void createFeedback() {
         // given
-        Long questId = 1L;
-        FeedbackRequest feedbackRequest = new FeedbackRequest("png1", 3);
+        final FeedbackRequest feedbackRequest = new FeedbackRequest(
+                "png1",
+                1L,
+                5
+        );
 
-        Member member = new Member(1L, "JohnDoe", "john123", SocialLoginType.KAKAO, "john@example.com",
-                "Developer", "Male", 1990, ENTHUSIAST, MEMBER, LEVEL1);
-        SelectedQuest selectedQuest = new SelectedQuest(member, new Quest(1L, "점심시간, 몸과 마음을 건강하게 유지하며",
-                "15분 운동하기", QuestType.BY_TYPE, 3, ENTHUSIAST, OUTSIDE_ALONE));
-        Feedback feedback = new Feedback(member, selectedQuest, feedbackRequest.getImageUrl(), feedbackRequest.getDifficulty());
-
-        when(selectedQuestRepository.findByQuestId(questId)).thenReturn(Optional.of(selectedQuest));
-        when(memberRepository.findById(member.getId())).thenReturn(Optional.of(member));
-        when(feedbackRepository.save(any(Feedback.class))).thenReturn(feedback);
+        given(memberRepository.findById(anyLong()))
+                .willReturn(Optional.of(GOEUN));
+        given(selectedQuestRepository.findById(anyLong()))
+                .willReturn(Optional.of(COMPLETED_QUEST1));
+        given(feedbackRepository.save(any(Feedback.class)))
+                .willReturn(FEEDBACK1);
 
         // when
-        FeedbackResponse feedbackResponse = feedbackService.createFeedback(questId, feedbackRequest);
+        final FeedbackResponse actualResponse = feedbackService.createFeedback(GOEUN.getId(), feedbackRequest);
 
         // then
-        assertEquals(feedback.getId(), feedbackResponse.getId());
-        assertEquals(feedback.getImageUrl(), feedbackResponse.getImageUrl());
-        assertEquals(feedback.getDifficulty(), feedbackResponse.getDifficulty());
-        assertEquals(feedback.getMember().getId(), feedbackResponse.getMemberId());
-        assertEquals(feedback.getSelectedQuest().getId(), feedbackResponse.getSelectedQuestId());
-
-        // Mock된 questService가 completeSelectedQuestWithFeedback을 호출할 때 상태 변경 검증
-        verify(questService, times(1)).completeSelectedQuestWithFeedback(selectedQuest, feedback);
-
-        // 상태 변경 검증
-        verify(questService, times(1)).completeSelectedQuestWithFeedback(selectedQuest, feedback);
-        selectedQuest.AddFeedbackToSelectedQuest(feedback);
-        assertEquals(QuestStatus.COMPLETED, selectedQuest.getQuestStatus());
+        verify(memberRepository).findById(any());
+        verify(selectedQuestRepository).findById(anyLong());
+        verify(feedbackRepository).save(any());
+        assertThat(actualResponse).usingRecursiveComparison()
+                .isEqualTo(FeedbackResponse.from(GOEUN));
     }
 
-    @DisplayName("존재하지 않는 questId로 피드백을 생성할 때 예외가 발생한다.")
+    @DisplayName("존재하지 않는 선택된 퀘스트 아이디로 피드백을 생성할 때 예외가 발생한다.")
     @Test
     void createFeedbackQuestNotFound() {
         // given
-        Long questId = 1L;
-        FeedbackRequest feedbackRequest = new FeedbackRequest("png2", 3);
+        final FeedbackRequest feedbackRequest = new FeedbackRequest(
+                "png1",
+                1L,
+                5
+        );
 
-        when(selectedQuestRepository.findByQuestId(questId)).thenReturn(Optional.empty());
+        given(memberRepository.findById(anyLong()))
+                .willReturn(Optional.of(GOEUN));
 
         // when & then
-        assertThrows(IllegalArgumentException.class, () -> feedbackService.createFeedback(questId, feedbackRequest));
+        assertThatThrownBy(() -> feedbackService.createFeedback(GOEUN.getId(), feedbackRequest))
+                .isInstanceOf(BadRequestException.class)
+                .extracting("code")
+                .isEqualTo(NOT_FOUND_SELECTED_QUEST_ID.getCode());
     }
 
-    @DisplayName("존재하지 않는 memberId로 피드백을 생성할 때 예외가 발생한다.")
+    @DisplayName("존재하지 않는 멤버 아이디로 피드백을 생성할 때 예외가 발생한다.")
     @Test
     void createFeedbackMemberNotFound() {
         // given
-        Long questId = 1L;
-        FeedbackRequest feedbackRequest = new FeedbackRequest("png3", 3);
-
-        Member member = new Member(1L, "JohnDoe", "john123", SocialLoginType.APPLE, "john@example.com",
-                "Developer", "Male", 1990, ENTHUSIAST, MEMBER, LEVEL1);
-        SelectedQuest selectedQuest = new SelectedQuest(member, new Quest(1L, "점심시간, 몸과 마음을 건강하게 유지하며",
-                "15분 운동하기", QuestType.BY_TYPE, 3, ENTHUSIAST, INSIDE_ALONE));
-
-        when(selectedQuestRepository.findByQuestId(questId)).thenReturn(Optional.of(selectedQuest));
-        when(memberRepository.findById(member.getId())).thenReturn(Optional.empty());
+        final FeedbackRequest feedbackRequest = new FeedbackRequest(
+                "png1",
+                1L,
+                5
+        );
 
         // when & then
-        assertThrows(IllegalArgumentException.class, () -> feedbackService.createFeedback(questId, feedbackRequest));
+        assertThatThrownBy(() -> feedbackService.createFeedback(GOEUN.getId(), feedbackRequest))
+                .isInstanceOf(BadRequestException.class)
+                .extracting("code")
+                .isEqualTo(NOT_FOUND_MEMBER_ID.getCode());
     }
 }
