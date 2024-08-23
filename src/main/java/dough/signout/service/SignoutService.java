8218b39f6e0dac6aab590278fb0905070103f26f
karl21-02 <manuna530@gmail.com>
2024-08-23 23:00:33 +0900

    [STYLE] 주석 제거

--- src/main/java/dough/signout/service/SignoutService.java
+++ src/main/java/dough/signout/service/SignoutService.java
@@ -32,7 +32,6 @@ public class SignoutService {
     private final FeedbackRepository feedbackRepository;
 
     public SignoutResponseDTO signout(SignoutRequestDTO signoutRequestDTO) {
-        // 토큰 인증 및 파싱
         String token =  signoutRequestDTO.getToken();
         Long memberId = null;
 
@@ -42,28 +41,23 @@ public class SignoutService {
 
         memberId = tokenProvider.getMemberIdFromToken(token);
 
-        // 멤버 객체 찾기
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new BadRequestException(NOT_FOUND_MEMBER_ID));
 
-        // 멤버 삭제하기
         memberRepository.delete(member);
 
-        // 퀘스트 삭제해 버리기
         List<SelectedQuest> selectedQuestList = member.getSelectedQuests();
         if (!selectedQuestList.isEmpty()) {
             selectedQuestList
                     .forEach(sq -> selectedQuestRepository.delete(sq));
         }
 
-        // 피드백 삭제해 버리기
         List<Feedback> feedbackList = member.getFeedbacks();
         if (!feedbackList.isEmpty()) {
             feedbackList
                     .forEach(fd -> feedbackRepository.delete(fd));
         }
 
-        // 알림 삭제해 버리기
         List<Notification> notificationList = member.getNotifications();
         if(!notificationList.isEmpty()) {
             notificationList
