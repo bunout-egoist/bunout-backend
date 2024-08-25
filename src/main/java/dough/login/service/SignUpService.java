131fb29aa29010ab7533c50412e57a82bfdd51e9
karl21-02 <manuna530@gmail.com>
2024-08-25 19:52:08 +0900

    [REFACTOR] 회원가입시 번아웃 유형, 고정 퀘스트 추가

--- src/main/java/dough/login/service/SignUpService.java
+++ src/main/java/dough/login/service/SignUpService.java
@@ -1,13 +1,22 @@
 package dough.login.service;
 
+import dough.burnout.domain.Burnout;
+import dough.burnout.domain.repository.BurnoutRepository;
+import dough.global.exception.BadRequestException;
 import dough.global.exception.UserNotFoundException;
 import dough.login.config.jwt.TokenProvider;
 import dough.login.dto.request.SignUpRequest;
 import dough.member.domain.Member;
 import dough.member.domain.repository.MemberRepository;
 import dough.member.dto.response.MemberInfoResponse;
+import dough.quest.domain.Quest;
+import dough.quest.domain.repository.QuestRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+import static dough.global.exception.ExceptionCode.*;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +24,8 @@ public class SignUpService {
 
     private final TokenProvider tokenProvider;
     private final MemberRepository memberRepository;
+    private final BurnoutRepository burnoutRepository;
+    private final QuestRepository questRepository;
 
     public MemberInfoResponse updateMemberInfo(SignUpRequest signUpRequest) {
         String accessToken = signUpRequest.getAccessToken();
@@ -29,6 +40,14 @@ public class SignUpService {
                 signUpRequest.getBirth_year(),
                 signUpRequest.getOccupation()
         );
+
+        Burnout burnout = burnoutRepository.findById(signUpRequest.getBunoutId())
+                .orElseThrow(() -> new BadRequestException(NOT_FOUND_BURNOUT_ID));
+        member.updateBurnout(burnout, LocalDate.now());
+
+        Quest fixedQuest = questRepository.findById(signUpRequest.getFixedQuestId())
+                .orElseThrow(() -> new BadRequestException(NOT_FOUND_QUEST_ID));
+        member.updateFixedQuest(fixedQuest, LocalDate.now());
 
         return MemberInfoResponse.of(memberRepository.save(member));
     }
