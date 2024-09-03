6d3ad1602deb28dcc0d90bea4bb950a35d895497
karl21-02 <manuna530@gmail.com>
2024-09-03 18:55:22 +0900

    [STYLE] 안쓰는 import문 제거

--- src/main/java/dough/feedback/dto/request/FeedbackRequest.java
+++ src/main/java/dough/feedback/dto/request/FeedbackRequest.java
@@ -7,7 +7,6 @@ import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.web.multipart.MultipartFile;
 
 @Getter
 @AllArgsConstructor
