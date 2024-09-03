5361ffc911734e00ec80d5d8bd7e75a2d3a68886
karl21-02 <manuna530@gmail.com>
2024-09-03 23:09:41 +0900

    [TEST] 피드백 테스트

--- src/test/java/dough/feedback/controller/FeedbackControllerTest.java
+++ src/test/java/dough/feedback/controller/FeedbackControllerTest.java
@@ -8,37 +8,30 @@ import dough.global.AbstractControllerTest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.multipart.MultipartFile;
 
-import static dough.global.restdocs.RestDocsConfiguration.field;
-import static dough.level.fixture.LevelFixture.LEVEL2;
-import static dough.member.fixture.MemberFixture.GOEUN;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
-import static org.springframework.http.HttpHeaders.AUTHORIZATION;
-import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
-import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
-import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.partWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParts;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(FeedbackController.class)
 @MockBean(JpaMetamodelMappingContext.class)
 @AutoConfigureRestDocs
 class FeedbackControllerTest extends AbstractControllerTest {
-
-    private static final String MEMBER_TOKENS = "Bearer accessToken";
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @MockBean
     private FeedbackService feedbackService;
@@ -55,57 +48,46 @@ class FeedbackControllerTest extends AbstractControllerTest {
     @Test
     void createFeedback() throws Exception {
         // given
-        final FeedbackRequest feedbackRequest = new FeedbackRequest(
-                "png1",
-                1L,
-                5
+        FeedbackRequest feedbackRequest = new FeedbackRequest(1L, 5);
+        MockMultipartFile feedbackRequestFile = new MockMultipartFile(
+                "feedback",
+                "",
+                "application/json",
+                new ObjectMapper().writeValueAsBytes(feedbackRequest)
         );
 
-        GOEUN.updateExp(40);
-        GOEUN.updateLevel(LEVEL2);
+        MockMultipartFile multipartFile = new MockMultipartFile(
+                "file",
+                "test.txt",
+                "text/plain",
+                "Test file content".getBytes()
+        );
 
-        final FeedbackResponse feedbackResponse = new FeedbackResponse(5, true);
+        // Mock 서비스 응답 설정
+        FeedbackResponse feedbackResponse = new FeedbackResponse(5, true, "https://asdkfl.com");
+        given(feedbackService.createFeedback(any(FeedbackRequest.class), any(MultipartFile.class)))
+                .willReturn(feedbackResponse);
 
-        when(feedbackService.createFeedback(any(FeedbackRequest.class)))
-                .thenReturn(feedbackResponse);
+        // Multipart 요청 전송
+        ResultActions resultActions = mockMvc.perform(multipart("/api/v1/feedbacks")
+                .file(feedbackRequestFile)  // 'feedback' 파트
+                .file(multipartFile)  // 'file' 파트
+                .contentType(MediaType.MULTIPART_FORM_DATA));
 
-        // when
-        ResultActions resultActions = mockMvc.perform(post("/api/v1/feedbacks")
-                .contentType(APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(feedbackRequest))
-                .header(AUTHORIZATION, MEMBER_TOKENS));
-
-        // then
+        // 응답 검증
         resultActions.andExpect(status().isOk())
-                .andDo(restDocs.document(
-                        requestHeaders(
-                                headerWithName("Authorization")
-                                        .description("엑세스 토큰")
-                        ),
-                        requestFields(
-                                fieldWithPath("selectedQuestId")
-                                        .type(NUMBER)
-                                        .description("선택된 퀘스트 아이디")
-                                        .attributes(field("constraint", "양의 정수")),
-                                fieldWithPath("imageUrl")
-                                        .type(STRING)
-                                        .description("이미지 URL")
-                                        .attributes(field("constraint", "문자열")),
-                                fieldWithPath("difficulty")
-                                        .type(NUMBER)
-                                        .description("난이도")
-                                        .attributes(field("constraint", "1-5 사이의 정수"))
+                .andDo(document(
+                        "create-feedback",
+                        requestParts(
+                                partWithName("feedback").description("피드백 요청 객체"),
+                                partWithName("file").description("파일")
                         ),
                         responseFields(
-                                fieldWithPath("currentLevel")
-                                        .type(NUMBER)
-                                        .description("현재 레벨")
-                                        .attributes(field("constraint", "양의 정수")),
-                                fieldWithPath("isLevelUp")
-                                        .type(BOOLEAN)
-                                        .description("레벨업 유무")
-                                        .attributes(field("constraint", "불리언"))
+                                fieldWithPath("currentLevel").type(NUMBER).description("현재 레벨"),
+                                fieldWithPath("isLevelUp").type(BOOLEAN).description("레벨업 유무"),
+                                fieldWithPath("imageUrl").type(STRING).description("이미지 경로")
                         )
                 ));
     }
+
 }
