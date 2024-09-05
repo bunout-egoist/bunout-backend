6c20de4c0d9754a667be65a4a960397b50b8fb72
karl21-02 <manuna530@gmail.com>
2024-09-05 15:39:37 +0900

    [REFACTOR] SignoutRequestDTO 수정

--- src/main/java/dough/signout/dto/request/SignoutRequestDTO.java
+++ src/main/java/dough/signout/dto/request/SignoutRequestDTO.java
@@ -1,8 +1,10 @@
 package dough.signout.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class SignoutRequestDTO {
     private String token;
 }
