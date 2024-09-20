a75c85734b080ac872f100ab2e1e0775fb3a4ff3
karl21-02 <manuna530@gmail.com>
2024-09-20 10:07:22 +0900

    [STYLE] 안쓰는 import 정리

--- src/main/java/dough/login/service/LoginService.java
+++ src/main/java/dough/login/service/LoginService.java
@@ -10,7 +10,6 @@ import dough.login.domain.LoginInfo;
 import dough.login.domain.MemberInfo;
 import dough.login.dto.request.SignUpRequest;
 import dough.login.dto.response.AccessTokenResponse;
-import dough.login.dto.response.AppleLoginResponse;
 import dough.login.dto.response.LoginResponse;
 import dough.login.infrastructure.jwt.TokenExtractor;
 import dough.login.infrastructure.jwt.TokenProvider;
